8fa6948f0444cfffec80bf827962afb8d3c3a7bc
HortDog <62645816+HortDog@users.noreply.github.com>
2024-04-06 02:46:48 +1000

    chat bot

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import tkinter as tk
+import PIL.Image
+import google.generativeai as genai
 
+# Set the API key for the generative AI
+GENERATIVEAI_API_KEY = 'AIzaSyDl6MHrxXPqXc5gfVArkdlgX9Nf0b9zzZ4'
+genai.configure(api_key=(GENERATIVEAI_API_KEY))
 
-def on_button_click():
-    label.config(text="Button clicked!")
+SELECTED_MODEL = 'gemini-pro'
+model = genai.GenerativeModel(SELECTED_MODEL)
+print('\n' + "UESING: " + SELECTED_MODEL)
 
-root = tk.Tk()
-
-label = tk.Label(root, text="Hello, World!")
-label.pack()
-
-button = tk.Button(root, text="Click me!", command=on_button_click)
-button.pack()
-
-root.mainloop()
+# chat with the AI, it will keep responding to the user input and remember the context of the conversation
+chat = model.start_chat()
+while True:
+  user_input = input("You: ")
+  
+  response = chat.send_message(user_input, stream=True)
+  print("Gemini: ")
+  for chunk in response:
+    print(chunk.text, end='', flush=True)
+  
+  print('\n')
